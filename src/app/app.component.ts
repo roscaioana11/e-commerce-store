8bf9e6fd681306352e56ecd57fc2bff14bc0ff91
Ioana Rosca <rosca.ioana11@gmail.com>
2023-01-15 14:28:57 +0200

    Project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    
+  `,
+  styles: []
 })
 export class AppComponent {
   title = 'e-commerce-store';
